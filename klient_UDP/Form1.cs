4c18c1e9e7cfde23214e40867378c87f084982fc
student <student@PC-223>
2017-05-25 15:56:47 +0200

    Dzialajacy klient UDP

--- klient_UDP/Form1.cs
+++ klient_UDP/Form1.cs
@@ -20,7 +20,24 @@ namespace klient_UDP
 
         private void przycisk_wyslij_Click(object sender, EventArgs e)
         {
+            string host = adres.Text;
+            int port = Convert.ToInt32(my_port.Value);
+            try
+            {
+                UdpClient klient = new UdpClient(host, port);
+                Byte[] dane = Encoding.ASCII.GetBytes(wiadomosc.Text);
+                klient.Send(dane, dane.Length);
+                info_o_polaczeniu.Items.Add("Wysłanie wiadomosci do hosta" + host + ": " + port);
+                klient.Close();
+            }
+            catch (Exception ex)
+            {
+                info_o_polaczeniu.Items.Add("Błąd: Nie udalo sie wysłac wiadomosci");
+                MessageBox.Show(ex.ToString(), " bład");
+            }
 
         }
+
+
     }
 }
